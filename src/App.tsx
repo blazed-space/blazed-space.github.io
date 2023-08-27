7c759c96d256d6d642a64c31b1044dfcc762eefc
Tyler Ruff <hello@blazed.space>
2023-08-27 16:54:54 -0400

    test deploy

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+    return (
+        <>
+            <div>
+                <p>
+                    blazed.space R&D
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default App;
