616e6f8e8561baf0bd864cc441d4d202b2b3b972
Tyler Ruff <hello@blazed.space>
2023-08-28 13:17:27 -0400

    fixes

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -5,40 +5,137 @@ function About() {
             <div>
                 <div>
                     <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
-                        <div className="max-w-3xl">
-                        <h2 className="text-3xl font-bold sm:text-4xl">
-                            About Blazed Space
-                        </h2>
-                        </div>
-
                         <div className="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-                        <div className="relative h-64 overflow-hidden sm:h-80 lg:h-full">
-                            <img
-                            alt="Party"
-                            src="https://images.unsplash.com/photo-1496843916299-590492c751f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80"
-                            className="absolute inset-0 h-full w-full object-cover"
-                            />
-                        </div>
+                            <div className="relative h-64 overflow-hidden sm:h-80 lg:h-full">
+                                <img
+                                alt="Beaker"
+                                src="/images/wallpaper-beaker.png"
+                                className="absolute inset-0 h-full w-full object-cover rounded-md"
+                                />
+                            </div>
 
-                        <div className="lg:py-16">
-                            <article className="space-y-4 text-gray-600">
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui
-                                hic atque tenetur quis eius quos ea neque sunt, accusantium soluta
-                                minus veniam tempora deserunt? Molestiae eius quidem quam repellat.
-                            </p>
-
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
-                                explicabo quidem voluptatum voluptas illo accusantium ipsam quis,
-                                vel mollitia? Vel provident culpa dignissimos possimus, perferendis
-                                consectetur odit accusantium dolorem amet voluptates aliquid,
-                                ducimus tempore incidunt quas. Veritatis molestias tempora
-                                distinctio voluptates sint! Itaque quasi corrupti, sequi quo odit
-                                illum impedit!
-                            </p>
-                            </article>
+                            <div className="lg:py-16">
+                                <article className="space-y-4 text-gray-600">
+                                    <h2 className="text-2xl font-bold sm:text-4xl">
+                                        About Us
+                                    </h2>
+                                    <p>
+                                        At Blazed Space, we're igniting possibilities in the digital realm, where innovation knows no bounds. 
+                                        Our passion for software development fuels us to explore new frontiers, creating solutions that redefine industries and shape the future.
+                                    </p>
+                                    <h3 className="text-xl">
+                                        Unleashing Innovation
+                                    </h3>
+                                    <p>
+                                        We're not just a software company; we're a beacon of innovation. 
+                                        Our diverse team of brilliant minds thrives on curiosity, excellence, and a relentless pursuit of pushing technological boundaries.
+                                    </p>
+                                    <h3 className="text-xl">
+                                        What We Do
+                                    </h3>
+                                    <ul className="list-disc pl-5">
+                                        <li>
+                                            <b>Cutting-Edge Applications:</b> Our applications are more than code—they're experiences. We build applications that redefine user engagement and elevate business success.
+                                        </li>
+                                        <li>
+                                            <b>Web Solutions:</b> From seamless websites to complex web applications, we're crafting digital journeys that captivate users and drive businesses forward.
+                                        </li>
+                                        <li>
+                                            <b>Data Insights:</b> Transforming data into actionable insights is our superpower. Our data-driven solutions empower businesses to make informed decisions.
+                                        </li>
+                                        <li>
+                                            <b>Innovative Technologies:</b> We harness emerging technologies like AI, blockchain, and AR to create solutions that not only meet today's needs but anticipate tomorrow's challenges.
+                                        </li>
+                                    </ul>
+                                </article>
+                            </div>
                         </div>
+                    </div>
+                </div>
+                <div className="m-4 md:m-8 pb-10 text-gray-800">
+                    <div className="container mx-auto p-4 my-6 space-y-2 text-center">
+                        <h3 className="text-4xl font-bold">
+                            Why Choose Us?
+                        </h3>
+                    </div>
+                    <div className="container mx-auto grid justify-center gap-4 sm:grid-cols-2 lg:grid-cols-3">
+                        <div className="flex flex-col items-center p-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-8 h-8 text-violet-600">
+                                <path fillRule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clipRule="evenodd"></path>
+                            </svg>
+                            <h3 className="my-3 text-xl font-semibold">
+                                Cutting-Edge Solutions
+                            </h3>
+                            <div className="space-y-1 leadi">
+                                <p>
+                                    We don't follow trends; we set them. Our software solutions are the embodiment of innovation, setting new benchmarks and transforming industries.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex flex-col items-center p-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-8 h-8 text-violet-600">
+                                <path fillRule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clipRule="evenodd"></path>
+                            </svg>
+                            <h3 className="my-3 text-xl font-semibold">
+                                Limitless Creativity
+                            </h3>
+                            <div className="space-y-1 leadi">
+                                <p>
+                                    We believe limitations are opportunities in disguise. Our team thrives on transforming complex challenges into elegant, user-centric solutions.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex flex-col items-center p-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-8 h-8 text-violet-600">
+                                <path fillRule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clipRule="evenodd"></path>
+                            </svg>
+                            <h3 className="my-3 text-xl font-semibold">
+                                Research-Driven Excellence
+                            </h3>
+                            <div className="space-y-1 leadi">
+                                <p>
+                                    Research isn't a step in our process; it's the foundation. We're dedicated to exploring the latest technologies and methodologies to deliver excellence.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex flex-col items-center p-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-8 h-8 text-violet-600">
+                                <path fillRule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clipRule="evenodd"></path>
+                            </svg>
+                            <h3 className="my-3 text-xl font-semibold">
+                                Global Collaboration
+                            </h3>
+                            <div className="space-y-1 leadi">
+                                <p>
+                                    Collaboration fuels innovation. We collaborate with talents from around the world, creating a tapestry of diverse ideas and expertise.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex flex-col items-center p-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-8 h-8 text-violet-600">
+                                <path fillRule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clipRule="evenodd"></path>
+                            </svg>
+                            <h3 className="my-3 text-xl font-semibold">
+                                Impactful Results
+                            </h3>
+                            <div className="space-y-1 leadi">
+                                <p>
+                                    We don't just write code; we craft digital experiences that leave a lasting impact on users, businesses, and society as a whole.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex flex-col items-center p-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-8 h-8 text-violet-600">
+                                <path fillRule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clipRule="evenodd"></path>
+                            </svg>
+                            <h3 className="my-3 text-xl font-semibold">
+                                Expert Team
+                            </h3>
+                            <div className="space-y-1 leadi">
+                                <p>
+                                    Our team of software engineers, designers, and visionaries are at the forefront of technology, working collaboratively to turn ideas into reality.
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
